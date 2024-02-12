6058d27e752ecc1279681bbab82b1d454c531306
Michael <michaeljrdeitz@gmail.com>
2024-02-12 16:04:56 -0600

    Code upload to git

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+fetch('https://randomfox.ca/floof/')
+    .then(response => response.json())
+    .then(
+        (convertedData) => {
+            const foxImgElement = document.querySelector("#fox")
+            foxImgElement.src = convertedData.image
+        }
+    )
+
+
+    fetch('http://shibe.online/api/cats?count=1&urls=true&httpUrls=false')
+    .then(response => response.json())
+    .then(convertedData =>{
+        const catImgElement = document.querySelector("#cat")
+        catImgElement.src =  convertedData[0]
+    })
